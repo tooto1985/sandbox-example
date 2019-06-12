fc8f58df9589316deeb5558beb7f5a465ffb05e4
Tommy <tooto1985@gmail.com>
2019-06-12 10:46:43 +0800

    first commit

---
+++ sandbox.js
@@ -0,0 +1,12 @@
+var aaa = 100;
+var bbb = 200;
+console.log("in sandbox aaa:", aaa);
+console.log("in sandbox bbb:", bbb);
+window.abc = 'abc';
+window.xyz = 'xyz';
+console.log("in sandbox window.abc:", window.abc);
+console.log("in sandbox window.xyz:", window.xyz);
+window.document.aaa=999
+
+
+
